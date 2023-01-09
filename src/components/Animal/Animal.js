605973c31dcc1ba18a806dc272dee12ca249791f
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-09 15:10:33 -0800

    animals display correctly

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,12 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  console.log(props);
+  return (
+    <div className="animal" style={{ top: `${props.top}`, left: `${props.left}` }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} alt={props.name}></img>
+      <h1>{props.name}</h1>
+      <p>{props.says}</p>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './Main.css';
+import Animal from '../Animal/Animal.js';
 import background from '../../background.png';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
 }

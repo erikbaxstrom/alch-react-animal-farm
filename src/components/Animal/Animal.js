d34d5a72be3f7c05b1330fad96a24cea6d749c36
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-09 15:22:32 -0800

    removed console logs

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,9 +1,8 @@
 import './Animal.css';
 
 export default function Animal(props) {
-  console.log(props);
   return (
-    <div className="animal" style={{ top: `${props.top}`, left: `${props.left}` }}>
+    <div className="animal" style={{ top: props.top, left: props.left }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} alt={props.name}></img>
       <h1>{props.name}</h1>
       <p>{props.says}</p>

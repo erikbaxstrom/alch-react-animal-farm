c472c18cce446b0b78967ee13db511df217545cd
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-10 17:25:14 -0800

    remove un-necessary env variable and mkae use of name class

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -3,8 +3,8 @@ import './Animal.css';
 export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
-      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} alt={props.name}></img>
-      <h1>{props.name}</h1>
+      <img src={`animals/${props.type}.svg`} alt={props.name}></img>
+      <span className="name">{props.name}</span>
       <p>{props.says}</p>
     </div>
   );
